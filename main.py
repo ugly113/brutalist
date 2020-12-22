4a3ecddb6b43467d091e631ad5ca01a1cdb46bae
ugly113 <jeffreyrstroup@gmail.com>
2020-12-21 20:41:06 -0500

    First attempt

---
+++ main.py
@@ -0,0 +1,35 @@
+import itertools
+
+
+characters = [
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+            'q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6',
+            '7','8','9','0','A','B','C','D','E','F','G','H','I','J','K','L',
+            'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+            ]
+
+password = input('Choose a 3 character password: ')
+
+
+def main():
+    cracked = ''
+    attempt = 0
+    per = itertools.permutations(characters, 3)
+    for val in per:
+        attempt = attempt +1
+        print(*val)
+        cracked = val
+        if password == cracked:
+            print(f'\n.:: {password} ::. Password broken after {attempt} attemps')
+
+"""    while password != cracked:
+        cracked = random.choice(characters)
+        cracked = cracked + random.choice(characters)
+        cracked = cracked + random.choice(characters)
+        attempt = attempt +1
+        print(cracked)
+    print(f'\n.:: {password} ::. Password broken after {attempt} attemps')
+"""
+
+if __name__ == '__main__':
+    main()
